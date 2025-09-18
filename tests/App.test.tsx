5273df6f848b00588bc73324c48c28286ee2f5aa
fcasco <fcasco@gmail.com>
2025-09-18 20:29:22 +0000

    test: adds basic somke tests

---
+++ tests/App.test.tsx
@@ -0,0 +1,9 @@
+import { describe, test } from 'vitest';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+describe('App', () => {
+  test('renders learn react link', () => {
+    render(<App />);
+  });
+});
